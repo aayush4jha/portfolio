5aab9ad036a3545537f29e0e65d7ff2c712db5a7
Aayush Jha <aayush4jha@gmail.com>
2025-05-22 09:53:37 +0530

    Navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import logo from "../assets/logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" className="h-10 w-auto" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <a href="https://www.linkedin.com/in/aayush4jha/">
+          <FaLinkedin className="text-white text-2xl hover:text-gray-300" />
+        </a>
+        <a href="https://github.com/aayush4jha">
+          <FaGithub className="text-white text-2xl hover:text-gray-300" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
