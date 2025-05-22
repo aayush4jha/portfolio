499effdacdf2cd0fd4eb43f3cf80e6d822052cc3
Aayush Jha <aayush4jha@gmail.com>
2025-05-22 12:36:55 +0530

    Hero Section Done :)

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import profilePic from '../assets/image.png';
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start lg:pl-20">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              Aayush Jha
+            </h1>
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+              Full Stack Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, Express.js and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.
+            </p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <img className="h-[400px]" src={profilePic} alt="Aayush Jha" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
